2d704623e2d0098e50e1d7fcbdbf1fbf469d74ef
shanto <shanto22e@gmail.com>
2025-05-21 10:38:27 +0600

    added post root

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv=require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.NAME}:${process.env.PASS}@cluster0.f1kjav4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const app=express()
 app.use(express.json())
@@ -11,6 +13,47 @@ const port=process.env.PORT || 3030
 app.get('/',(req,res) => {
     res.send('Hello World')
 })
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const groups=client.db('groupdb').collection('groups')
+
+    app.post('/groups',async (req,res) => {
+        const result= await groups.insertOne(req.body)
+        res.send(result)
+    })
+    
+    app.get('/groups',async (req,res) => {
+        const result= await groups.find().toArray()
+        res.send(result)
+    })
+    app.delete('/groups/:id',async (req,res) => {
+        const _id = new ObjectId(req.params.id)
+        const result= await groups.deleteOne({_id})
+        res.send(result)
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.listen(port,() => {
